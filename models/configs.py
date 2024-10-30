9178034589aa036fd61d5b208a4ad5315662f815
Specul <38744963+Snowinbio@users.noreply.github.com>
2024-10-30 16:11:40 +0800

    Update configs.py

--- models/configs.py
+++ models/configs.py
@@ -7,11 +7,11 @@ def get_LLNM_Net_config():
     config.transformer = ml_collections.ConfigDict()
     config.transformer.mlp_dim = 3072
     config.transformer.num_heads = 12
-    config.transformer.num_layers = 16
-    config.transformer.attention_dropout_rate = 0.2 # 0.0 - 0.2
-    config.transformer.dropout_rate = 0.3 # 0.1 - 0.3
+    config.transformer.num_layers = 16                # 16
+    config.transformer.attention_dropout_rate = 0.2   # 0.0 - 0.2
+    config.transformer.dropout_rate = 0.3             # 0.1 - 0.3
     config.classifier = 'token'
     config.representation_size = None
-    config.rr_len = 300
-    config.img_feature_len = 2   
+    config.rr_len = 300                               # token length
+    config.img_feature_len = 2                        # shape, echo
     return config
