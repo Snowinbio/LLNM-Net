c5821ca5ecc8632b759ae9f6bde29881988ceed6
Symcal <38744963+Snowinbio@users.noreply.github.com>
2025-10-20 22:20:31 +0800

    Update Echogenicity.py

--- Feature extraction/Morphology/model/Echogenicity.py
+++ Feature extraction/Morphology/model/Echogenicity.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 from skimage import io, transform, color, morphology as sm
 from scipy.stats import norm
-
+from scipy.optimize import minimize_scalar
 
 class Echogenicity:
     def __init__(self, benign_img_dir, benign_edge_dir, malignant_img_dir, malignant_edge_dir, echo_train_bool):
@@ -11,6 +11,7 @@ class Echogenicity:
         self.malignant_img_dir = malignant_img_dir
         self.malignant_edge_dir = malignant_edge_dir
         self.echo_train_bool = echo_train_bool
+        self.img_name_list = []
 
     def get_nodule_echo(self, path, jpg_img, img_bound, dilation_value=3):
         img = io.imread(path)
@@ -58,8 +59,46 @@ class Echogenicity:
             echo_sub = self.get_nodule_echo(img_path, jpg_img, img_bound)
             if echo_sub is not None:
                 echo_sub_list.append(echo_sub)
+                self.img_name_list.append(img_name)
 
         return echo_sub_list
+
+    def gaussian(self, x, mu, sigma):
+        coefficient = 1 / (sigma * np.sqrt(2 * np.pi))  # 归一化系数
+        exponent = - (x - mu) ** 2 / (2 * sigma ** 2)   # 指数部分
+        return coefficient * np.exp(exponent)
+    
+    def diff_a_minus_b(self, x, mu_a, sigma_a, mu_b, sigma_b):
+        a = self.gaussian(x, mu_a, sigma_a)
+        b = self.gaussian(x, mu_b, sigma_b)
+        return a - b
+    
+    def diff_b_minus_a(self, x, mu_a, sigma_a, mu_b, sigma_b):
+        return -self.diff_a_minus_b(x, mu_a, sigma_a, mu_b, sigma_b)
+
+    def find_max_diff(self, mu_a, sigma_a, mu_b, sigma_b, search_range):
+        def neg_diff_func(x):
+            return -self.diff_func(x, mu_a, sigma_a, mu_b, sigma_b)
+        result = minimize_scalar(neg_diff_func, bounds=search_range, method='bounded')
+        if result.success:
+            max_x = result.x  # 最大值对应的x
+            max_diff = self.diff_func(max_x, mu_a, sigma_a, mu_b, sigma_b)  # 最大差值
+            return max_x, max_diff
+        else:
+            raise ValueError(f"Error in {result.message}")
+        
+    def calculate_range(self, mu_a, sigma_a, mu_b, sigma_b):
+        search_left = min(mu_a, mu_b) - 3 * max(sigma_a, sigma_b)
+        search_right = max(mu_a, mu_b) + 3 * max(sigma_a, sigma_b)
+        search_range = (search_left, search_right)
+
+        max_x_a_gt_b, max_diff_a_gt_b = self.find_max_diff(
+            self.diff_a_minus_b, mu_a, sigma_a, mu_b, sigma_b, search_range
+        )
+        max_x_b_gt_a, max_diff_b_gt_a = self.find_max_diff(
+            self.diff_b_minus_a, mu_a, sigma_a, mu_b, sigma_b, search_range
+        )
+        return max_diff_a_gt_b, max_diff_b_gt_a
 
     def calculate_statistics(self):
         benign_echo_sub_list = self.process_images(self.benign_img_dir, self.benign_edge_dir)
@@ -83,6 +122,10 @@ class Echogenicity:
             sigma_benign = 18.219738470294324
             mu_malignant = -59.779479141140925
             sigma_malignant = 17.421576680834857
+            self.benign_range = 0.00215678
+            self.malignant_range = 0.00234567
+        else:
+            self.benign_range,self.malignant_range = self.calculate_range(mu_benign, sigma_benign, mu_malignant, sigma_malignant)
 
         return benign_echo_sub_list, malignant_echo_sub_list, mu_benign, sigma_benign, mu_malignant, sigma_malignant
 
@@ -94,8 +137,14 @@ class Echogenicity:
         for i in echo_sub_list:
             p_malignant = norm.pdf(i, mu_malignant, sigma_malignant)
             p_benign = norm.pdf(i, mu_benign, sigma_benign)
-            p_norm_echo.append(p_malignant - p_benign)
+            p_echo = p_malignant - p_benign
+            if p_echo > 0:
+                p_norm_echo.append(0.5+p_echo/(2*self.malignant_range))
+            else:
+                p_norm_echo.append(0.5+p_echo/(2*self.benign_range))
 
-        return p_norm_echo
+        p_norm_echo_dict = {}
+        for i in range(len(self.img_name_list)):
+            p_norm_echo_dict[self.img_name_list[i]] = p_norm_echo[i]
 
-
+        return p_norm_echo_dict
